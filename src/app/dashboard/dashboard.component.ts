95dbbc34df737585e05c23c55c5875195bc430e4
madibek1992@mail.ru <ayupov.m.b@gmail.com>
2018-07-11 00:48:50 +0600

    changes to deploy on github pages

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  title = 'bek';
+  title = 'Ассалаумағалейкум, достар';
 
   constructor() { }
 
